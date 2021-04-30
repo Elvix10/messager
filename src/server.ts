e8aefcedd720de9ed7c33f74063ca6f5ea03b627
Bruno Vaz <brunel55@hotmail.com>
2021-04-30 09:15:16 -0100

    public folder with frontend added

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express from 'express'
 import {routes} from "./routes"
+import {createServer} from 'http'
+import { Server, Socket } from 'socket.io'
 import "./database"
 
 const app=express()
+
+const http= createServer(app)
+const io =new Server(http)
+
+io.on("connection",(socket:Socket)=>{
+    console.log("connected",socket.id);
+    
+})
 
 app.use(express.json())
 app.use(routes)
